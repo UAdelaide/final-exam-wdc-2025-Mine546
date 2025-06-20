0056ebe560405e90ea9a795e6d695c287a57fef4
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 07:52:44 +0000

    [automatic commit] 2025-06-20 07:52:43 UTC 115.131.49.185

--- part1/app.js
+++ part1/app.js
@@ -13,8 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-let db;
-
 (async () => {
   try {
     // Connect to MySQL without specifying a database
