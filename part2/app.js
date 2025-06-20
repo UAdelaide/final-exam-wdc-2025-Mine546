216bd12934396491e3c53e866c35fca8cb44e5b4
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 12:35:11 +0000

    [automatic commit] 2025-06-20 12:35:11 UTC 115.131.49.185

--- part2/app.js
+++ part2/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
@@ -7,14 +6,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-
-
-
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+// eslint-disable-next-line linebreak-style
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
