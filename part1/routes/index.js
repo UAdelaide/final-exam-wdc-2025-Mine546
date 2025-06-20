311dddb449f049fb922264793f6f673cbb0ef65c
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 07:30:04 +0000

    [automatic commit] 2025-06-20 07:30:03 UTC 115.131.49.185

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,11 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-router.get('/api/dogs', function(req, res, next){
-  
-
-
-});
-
 module.exports = router;
