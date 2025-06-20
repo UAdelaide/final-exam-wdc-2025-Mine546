e88cda8f93726e6f61d49d46f6429e2ac99b9d14
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 07:30:24 +0000

    [automatic commit] 2025-06-20 07:30:24 UTC 115.131.49.185

--- part1/app.js
+++ part1/app.js
@@ -15,6 +15,16 @@ app.use(cookieParser());
 
 
 
+
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [books] = await db.execute('SELECT * FROM books');
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
