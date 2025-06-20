78d6698c99a0f4b26bde8aefab7032499c474e37
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 13:06:37 +0000

    [automatic commit] 2025-06-20 13:06:37 UTC 115.131.49.185

--- part2/app.js
+++ part2/app.js
@@ -7,6 +7,8 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+//new
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
