6311db3450ee3c2a6e10434f7d48fd5654e71681
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 11:16:59 +0000

    [automatic commit] 2025-06-20 11:16:58 UTC 115.131.49.185

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -12,9 +12,12 @@ const app = Vue.createApp({
                 const res = await fetch('https://dog.ceo/api/breeds/image/random');
                 const data = await res.json();
                 this.dogImage = data.message;
-            } catch
+            } catch (error) {
+                console.error('whoops', error);
+            }
         }
-    }
+    },
+    
 })
 
 app.mount('#app');
