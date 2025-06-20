7df1a4c197abcb16983f09b917bc456a334aac7c
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 07:58:40 +0000

    [automatic commit] 2025-06-20 07:58:40 UTC 115.131.49.185

--- part1/app.js
+++ part1/app.js
@@ -59,7 +59,5 @@ app.get('/api/walkers/summary', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
