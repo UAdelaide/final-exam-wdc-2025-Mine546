29da589ac0e11410233b9bbec71ff1bfe80ee170
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 13:17:37 +0000

    [automatic commit] 2025-06-20 13:17:36 UTC 115.131.49.185

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -53,6 +53,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'no password' });
     }
     if (rows.length === 0) {
+      
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
