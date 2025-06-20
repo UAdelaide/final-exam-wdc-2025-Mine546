9815464bcf09ffa17467764bcd95a4150cea3a9e
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 09:52:00 +0000

    [automatic commit] 2025-06-20 09:51:59 UTC 115.131.49.185

---
+++ part1/public/javascripts/vue.js
@@ -0,0 +1,22 @@
+const vueinst = new Vue({
+    el: '#app',
+    data: {
+        choose: 'Choose ...',
+        special: SPECIALS[0],
+        show_ad: true,
+        top_menu: MENU,
+        top_menu_item: null,
+        top_menu_hover: false
+    },
+    methods: {
+        changeChoose(newChoose) {
+            this.choose = newChoose;
+        },
+        setTopMenuItem(index) {
+            this.top_menu_item = index;
+        },
+        setTopMenuHover(state) {
+            this.top_menu_hover = state;
+        }
+    }
+});
