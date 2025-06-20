88214ecdb31dfd15af2d8e9418ef1da1b86b3d72
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 07:55:31 +0000

    [automatic commit] 2025-06-20 07:55:31 UTC 115.131.49.185

--- part1/app.js
+++ part1/app.js
@@ -15,18 +15,7 @@ app.use(cookieParser());
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('USE DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
+    
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
@@ -34,7 +23,7 @@ app.use(cookieParser());
       database: 'DogWalkService'
     });
 
-    
+
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
