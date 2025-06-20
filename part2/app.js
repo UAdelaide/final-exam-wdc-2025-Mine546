ec5cde124676cddcae3005597af0f32193b6bfc3
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-20 13:37:10 +0000

    [automatic commit] 2025-06-20 13:37:10 UTC 115.131.49.185

--- part2/app.js
+++ part2/app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const session = require('express-session');
 
 app.use(session({
+  secret: 'yug'
   resave: false,
   saveUninitialized: false,
   cookie: { secure: false }
