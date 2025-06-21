e452b4ca8c21efaa829d2ef4b80111e969cea64f
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-21 03:01:45 +0000

    [automatic commit] 2025-06-21 03:01:45 UTC 115.131.49.185

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,7 +88,7 @@ router.get('/owner-dashboard.html', async (req, res) => {
     for (let i=0; i<rows.length; i++){
       const sel = document.getElementById("dogName");
       const el = document.getElement("option");
-      sel.add(el, sel.)
+      sel.add(el, sel.options[1]);
     }
 
 
