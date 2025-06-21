b6ef7ecc2f5bb624e4c97b783f2bf94fd73d1a8d
Mine546 <105634840+Mine546@users.noreply.github.com>
2025-06-21 07:17:41 +0000

    [automatic commit] 2025-06-21 07:17:41 UTC 115.131.49.185

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -30,7 +30,6 @@ router.post('/register', async (req, res) => {
 });
 
 router.get('/me', (req, res) => {
-  console.log(req.session.userId);
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
